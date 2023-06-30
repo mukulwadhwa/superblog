e91eea4adbce83c9f8c3bbcb01ca0100b72b8982
Mukul Wadhwa <mukulwadhwa99@gmail.com>
2023-07-01 03:42:35 +0530

    Login Page Created

--- superblogapp/views.py
+++ superblogapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 def index(request):
@@ -8,4 +9,30 @@ def my_view(request):
     context = {
         'message': 'Hello, World!'
     }
-    return render(request, 'index.html', context)
+    return render(request, 'login.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username, password=password)
+            # Perform additional authentication checks if necessary
+            # For example, you could use Django's built-in authentication system or third-party libraries
+            return redirect('home')  # Redirect to the home page after successful login
+        
+        except User.DoesNotExist:
+            error = 'Invalid username or password'
+            return render(request, 'login.html', {'error': error})
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to the login page after successful signup
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
